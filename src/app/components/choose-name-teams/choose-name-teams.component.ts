7cc6113e36e42c49a7155e8c71d90a8461957dd8
Minuke <nakamadeath@gmail.com>
2023-12-30 11:58:00 +0100

    form for two teams

--- src/app/components/choose-name-teams/choose-name-teams.component.ts
+++ src/app/components/choose-name-teams/choose-name-teams.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormTwoTeamsComponent } from '../form-two-teams/form-two-teams.component';
 
 @Component({
   selector: 'app-choose-name-teams',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormTwoTeamsComponent],
   templateUrl: './choose-name-teams.component.html',
   styleUrl: './choose-name-teams.component.scss'
 })
@@ -13,18 +14,4 @@ export class ChooseNameTeamsComponent {
 
   @Input() teams!: number;
 
-  constructor(private fb: FormBuilder) { }
-
-  teamForm = this.fb.group({
-    name: ['', Validators.required],
-    score: [0]
-  });
-
-  get name() {
-    return this.teamForm.get('name');
-  }
-
-  onSubmit() {
-    console.log(this.teamForm.value);
-  }
 }
