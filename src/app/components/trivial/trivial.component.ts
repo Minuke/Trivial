9d179299e6aa902ca4a083c6df7e7114f5f34278
Minuke <nakamadeath@gmail.com>
2023-12-30 15:56:13 +0100

    add points to the team when the answer is correct

--- src/app/components/trivial/trivial.component.ts
+++ src/app/components/trivial/trivial.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
+import { Team } from 'app/interfaces/team.interface';
 import { Answer, Trivial } from 'app/interfaces/trivial.interface';
 import { GameService } from 'app/services/game.service';
 
@@ -15,10 +16,35 @@ export class TrivialComponent {
   private gameService:GameService = inject(GameService);
 
   @Input() question!:Trivial;
+  @Input() teams!:Team[];
+
+  public currentTurn = 0;
+  public currentTeam!: Team;
 
   selectAnswer(answer:Answer):void {
+    this.nextTeamTurn();
+    this.saveAnswerStatus(answer);
+    this.addPoints(answer)
+  }
+
+  saveAnswerStatus(answer:Answer) {
     answer.selected = true;
     this.gameService.setQuestion(this.question);
+  }
+
+  nextTeamTurn():void {
+    if (this.currentTurn >= this.teams.length) {
+      this.currentTurn = 0;
+    }
+    this.currentTeam = this.teams[this.currentTurn];
+    this.currentTurn++;
+  }
+
+  addPoints(answer:Answer):void {
+    if(answer.correct) {
+      this.currentTeam.score += 1;
+      this.gameService.setTeamData(this.teams);
+    }
   }
 
 }
