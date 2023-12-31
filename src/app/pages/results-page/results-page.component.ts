91e79f3a57cac9c3be594ab9067f759342ef1a4b
Minuke <nakamadeath@gmail.com>
2023-12-31 15:08:48 +0100

    basic structure of the results page

--- src/app/pages/results-page/results-page.component.ts
+++ src/app/pages/results-page/results-page.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { GameWinnerComponent } from "app/components/game-winner/game-winner.component";
+import { ScoreBoardComponent } from "app/components/score-board/score-board.component";
+import { Team } from "app/interfaces/team.interface";
+import { GameService } from "app/services/game.service";
+
 
 @Component({
   selector: 'app-results-page',
   standalone: true,
-  imports: [],
+  imports: [ScoreBoardComponent, GameWinnerComponent],
   templateUrl: './results-page.component.html',
   styleUrl: './results-page.component.scss'
 })
 export class ResultsPageComponent {
+
+  public teams: Team[] = [];
+  private gameService:GameService = inject(GameService);
+
+  ngOnInit():void {
+    this.teams = this.gameService.getTeamData();
+    console.log(this.teams)
+  }
 
 }
